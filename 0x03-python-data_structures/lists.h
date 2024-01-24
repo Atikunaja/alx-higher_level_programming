f70a79afa11a02e319957d613e2e6e4b3563d3bc
Atikunaja <najaatiku1@gmail.com>
2024-01-23 20:40:35 -0800

    palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -41,6 +41,9 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
+void reverse_listint(listint_t **head);
+
+int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
 
