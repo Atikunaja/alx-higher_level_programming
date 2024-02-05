64a347d3fae4d7251b09eec10fd1435a9d71e772
Atikunaja <najaatiku1@gmail.com>
2024-02-05 09:59:47 -0800

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    Return
+    try:
+        if len(my_list) < x:
+            print("".join(my_list))
+            return x - len(my_list)
+    except IndexError:
+        print("The list doesn't have enough elements")
